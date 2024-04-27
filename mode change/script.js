bf884fc905e74609a1eb8301467fcbcdc16d1022
RIMNIK <165829066+RIMNIK@users.noreply.github.com>
2024-04-27 16:37:09 +0530

    Add files via upload

---
+++ mode change/script.js
@@ -0,0 +1,20 @@
+let btnMode = document.querySelector("#btn");
+let body = document.querySelector("body");
+let currMode = "light";
+
+btnMode.addEventListener("click" , () => {
+    if (currMode == "light")
+    {
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else
+    {
+        currMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+
+    console.log(currMode)
+});
